1002888d68bcf68c84ad2b8de7c5db6719d26e90
Connor Cowan <ccowan003@comcast.net>
2019-06-06 01:27:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  sentence.split.collect {|word|
+  sentence.split.collect {|word|          #.split => string to array
     word.reverse
   }
   .join(" ")              #converts array to string
